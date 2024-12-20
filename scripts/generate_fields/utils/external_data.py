b9451704f55edc947ac78ac89a10dca5e4d9923e
Jonathan Brockett <joncbrockett@gmail.com>
2024-12-20 13:15:57 -0800

    made sure env variable referenced in yml

--- scripts/generate_fields/utils/external_data.py
+++ scripts/generate_fields/utils/external_data.py
@@ -1,10 +1,8 @@
 import requests, os, logging
 from utils.prompt import README_PROMPT
 
-PAT = os.getenv("GITHUB_EMU_PAT")
-EMU_PAT = os.getenv("GITHUB_EMU_PAT")
+EMU_PAT = os.getenv("GIT_EMU_PAT")
 
-HEADERS = {'Authorization': f'token {PAT}'} if PAT else {}
 EMU_HEADERS = {'Authorization': f'token {EMU_PAT}'} if EMU_PAT else {}
 
 def get_readme(github_url: str) -> str:
@@ -18,9 +16,7 @@ def get_readme(github_url: str) -> str:
                 logging.info(f"Getting correct branch and readme from {github_url}")
                 github_api = github_url.replace(raw_github, "https://api.github.com/repos")
 
-                res = requests.get(f"{github_api}/contents", headers=HEADERS)
-                if res.status_code != 200:
-                    res = requests.get(f"{github_api}/contents", headers=EMU_HEADERS)
+                res = requests.get(f"{github_api}/contents", headers=EMU_HEADERS)
                 contents = res.json()
                 readme_file = [file['download_url'] for file in contents if 'readme' in file['name'].lower()][0]
                 logging.info(f"Getting data from {readme_file}")
